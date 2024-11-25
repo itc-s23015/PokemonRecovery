b83e9343e57480000d883cf4633a58714658150c
Nakasone, Tsuyoshi <nakasone@std.it-college.ac.jp>
2024-11-25 14:25:19 +0900

    たぶん、世代とポケモンを紐づけるテーブルに関連の定義は出来ているはず。2

--- app/src/main/java/jp/ac/it_college/std/s23000/android/pokequiz/data/PokeQuizDatabase.kt
+++ app/src/main/java/jp/ac/it_college/std/s23000/android/pokequiz/data/PokeQuizDatabase.kt
@@ -5,6 +5,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import jp.ac.it_college.std.s23000.android.pokequiz.data.dao.GenerationDao
 import jp.ac.it_college.std.s23000.android.pokequiz.data.dao.PokemonDao
+import jp.ac.it_college.std.s23000.android.pokequiz.data.dao.PokemonIntroducedGenerationDao
 import jp.ac.it_college.std.s23000.android.pokequiz.data.entity.GenerationEntity
 import jp.ac.it_college.std.s23000.android.pokequiz.data.entity.PokemonEntity
 import jp.ac.it_college.std.s23000.android.pokequiz.data.entity.PokemonIntroducedGenerationCrossRef
@@ -24,4 +25,5 @@ import jp.ac.it_college.std.s23000.android.pokequiz.data.entity.PokemonIntroduce
 abstract class PokeQuizDatabase : RoomDatabase() {
     abstract val generationDao: GenerationDao
     abstract val pokemonDao: PokemonDao
+    abstract val pokemonIntroducedGenerationDao: PokemonIntroducedGenerationDao
 }
--- app/src/main/java/jp/ac/it_college/std/s23000/android/pokequiz/di/DatabaseModule.kt
+++ app/src/main/java/jp/ac/it_college/std/s23000/android/pokequiz/di/DatabaseModule.kt
@@ -10,6 +10,7 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import jp.ac.it_college.std.s23000.android.pokequiz.data.PokeQuizDatabase
 import jp.ac.it_college.std.s23000.android.pokequiz.data.repository.GenerationsRepository
+import jp.ac.it_college.std.s23000.android.pokequiz.data.repository.PokemonIntroducedGenerationRepository
 import jp.ac.it_college.std.s23000.android.pokequiz.data.repository.PokemonRepository
 import jp.ac.it_college.std.s23000.android.pokequiz.data.repository.impl.GenerationsRepositoryImpl
 import jp.ac.it_college.std.s23000.android.pokequiz.data.repository.impl.PokemonRepositoryImpl
@@ -34,6 +35,11 @@ object DatabaseModule {
     @Singleton
     @Provides
     fun providePokemonDao(db: PokeQuizDatabase) = db.pokemonDao
+
+    @Singleton
+    @Provides
+    fun providePokemonIntroducedGenerationDao(db: PokeQuizDatabase) =
+        db.pokemonIntroducedGenerationDao
 }
 
 @Module
@@ -44,4 +50,7 @@ abstract class RepositoryModule {
 
     @Binds
     abstract fun bindPokemonRepository(impl: PokemonRepositoryImpl): PokemonRepository
+
+    @Binds
+    abstract fun bindPokemonIntroducedGenerationRepository(impl: GenerationsRepositoryImpl): PokemonIntroducedGenerationRepository
 }
